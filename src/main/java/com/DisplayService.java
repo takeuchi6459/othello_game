59a73fe4759a5ff848293b4088e97709cf8c2b61
takeuchi6459 <takeuchi6459@yahoo.co.jp>
2024-08-14 08:13:51 +0000

    出力された配列の更新テスト

--- src/main/java/com/DisplayService.java
+++ src/main/java/com/DisplayService.java
@@ -36,7 +36,7 @@ public class DisplayService {
 
     }
 
-    public void BoradProt( String[][] brd ){
+    public void BoardProt( String[][] brd ){
 
         for (int i = 0; i < brd.length; i++) {
             for (int j = 0; j < brd[i].length; j++) {
@@ -45,6 +45,22 @@ public class DisplayService {
             System.out.println();
           }
 
+    }
+
+    public void BoardUpdate( String[][] brd ){
+      
+        for (int i = 0; i < brd.length; i++) {
+            System.out.print("\033[1A"); // カーソルを1行上に移動
+            System.out.print("\033[2K"); // 現在の行をクリア
+          }
+
+        for (int i = 0; i < brd.length; i++) {
+            for (int j = 0; j < brd[i].length; j++) {
+              System.out.print(brd[i][j] + " ");
+            }
+            System.out.println();
+          }
+  
     }
     
 }
--- src/main/java/com/OthelloMain.java
+++ src/main/java/com/OthelloMain.java
@@ -8,6 +8,7 @@ public class OthelloMain {
         // disp.OneLiner("Hello");
 
         String[][] list = new String[3][3];
+
         list[0][0]="1";
         list[0][1]="2";
         list[0][2]="3";
@@ -17,8 +18,43 @@ public class OthelloMain {
         list[2][0]="7";
         list[2][1]="8";
         list[2][2]="9";
+        disp.BoardProt( list );
+
+        // 少し待つ
+        Thread.sleep(1000);
 
 
-        disp.BoradProt( list );
+        for(int i=0;i < 10;i++){
+
+            list[0][0]="a";
+            list[0][1]="b";
+            list[0][2]="c";
+            list[1][0]="d";
+            list[1][1]="e";
+            list[1][2]="f";        
+            list[2][0]="g";
+            list[2][1]="h";
+            list[2][2]="i";
+            disp.BoardUpdate( list );
+
+            // 少し待つ
+            Thread.sleep(1000);
+
+            list[0][0]="1";
+            list[0][1]="2";
+            list[0][2]="3";
+            list[1][0]="4";
+            list[1][1]="5";
+            list[1][2]="6";        
+            list[2][0]="7";
+            list[2][1]="8";
+            list[2][2]="9";
+            disp.BoardUpdate( list );
+    
+            // 少し待つ
+            Thread.sleep(1000);
+            
+        }
+
     }
 }
