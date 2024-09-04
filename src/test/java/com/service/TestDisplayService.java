444a57da9b972df32372a02af8a965a93c530f51
takeuchi6459 <takeuchi6459@yahoo.co.jp>
2024-09-04 03:42:48 +0000

    JUnit5に変更

--- src/test/java/com/service/TestDisplayService.java
+++ src/test/java/com/service/TestDisplayService.java
@@ -3,15 +3,17 @@ package com.service;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestDisplayService {
     @ParameterizedTest
     @ValueSource(strings = {"Java", "Python"})
-    public void DisplayInitTest(){
+    public void DisplayInitTest(String s){
         DisplayService dp = new DisplayService();
-        for( int i = 0; i < 10; i++){
-            String temp = dp.Testmth("Test" + i);
-            assertEquals("Test" + i, temp);
-        }   
+        System.out.println("Value Source " + s + " test");
+        String temp = dp.Testmth(s);
+        assertEquals("Java", temp);
+
     }
 }
