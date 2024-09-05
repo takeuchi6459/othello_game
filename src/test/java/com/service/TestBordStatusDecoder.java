c54b3610263b10255d8b1d93bc6c106fc0a8a2a7
takeuchi6459 <takeuchi6459@yahoo.co.jp>
2024-09-05 03:52:56 +0000

    複数のテスト値入力に対応

--- src/test/java/com/service/TestBordStatusDecoder.java
+++ src/test/java/com/service/TestBordStatusDecoder.java
@@ -2,22 +2,25 @@ package com.service;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestBordStatusDecoder {
     
-    @Test
-    public void FirstTest(){
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4})
+    public void FirstTest(int s){
         BordStatusDecoder bd = new BordStatusDecoder();
-        assertEquals("●", bd.BordStatusDecoder(65535,2));
+        assertEquals("●", bd.BordStatusDecoder(s,7));
         // for(int i=0; i<8; i++){
         //     System.out.print(bd.BordStatusDecoder(65535,i));
         // }
     }
 
-    @Test
-    public void BordTest(){
-        EightGridBordDecoder EBD = new EightGridBordDecoder();
-        
-    }
+    // @ParameterizedTest
+    // @ValueSource(ints = {1, 2, 100,})
+    // public void BordTest(){
+    //     EightGridBordDecoder EBD = new EightGridBordDecoder();
+    //     EightGridBordDecoder(int[] n);
+    // }
 }
