c1cbbd964be760148baeba5ef64c1718fb260cda
takeuchi6459 <takeuchi6459@yahoo.co.jp>
2024-09-27 07:42:56 +0000

    ディスプレイの初期化とクリア関数を追加

--- src/main/java/com/service/DisplayService.java
+++ src/main/java/com/service/DisplayService.java
@@ -2,45 +2,7 @@ package com.service;
 
 public class DisplayService {
 
-    public void OneLiner(String msg){
-
-        System.out.println( msg );
-
-    }
-
-    public String Testmth(String msg){
-      return msg;
-    }
-
-
-    public void CarsorTest() throws InterruptedException{
-
-        // 初期の出力
-        System.out.println("Line 1: Initial text");
-        System.out.println("Line 2: Initial text");
-        System.out.println("Line 3: Initial text");
-
-        // 少し待つ
-        Thread.sleep(2000);
-
-        // カーソルを上に移動
-        System.out.print("\033[1A"); // カーソルを3行上に移動
-        System.out.print("\033[2K"); // 現在の行をクリア
-
-        System.out.print("\033[1A"); // カーソルを2行上に移動
-        System.out.print("\033[2K"); // 現在の行をクリア
-
-        System.out.print("\033[1A"); // カーソルを1行上に移動
-        System.out.print("\033[2K"); // 現在の行をクリア
-
-        // 新しい内容を出力
-        System.out.println("Updated Line 1");
-        System.out.println("Updated Line 2");
-        System.out.println("Updated Line 3");
-
-    }
-
-    public void BoardProt( String[][] brd ){
+    public void displayProt( String[][] brd ){
 
         for (int i = 0; i < brd.length; i++) {
             for (int j = 0; j < brd[i].length; j++) {
@@ -51,7 +13,7 @@ public class DisplayService {
 
     }
 
-    public void BoardUpdate( String[][] brd ){
+    public void DisplayUpdate( String[][] brd ){
       
         for (int i = 0; i < brd.length; i++) {
             System.out.print("\033[1A"); // カーソルを1行上に移動
@@ -65,6 +27,23 @@ public class DisplayService {
             System.out.println();
           }
   
+    }
+
+    public void DisplayInit( int hight ){
+
+      for (int i = 0; i < hight; i++) {
+        System.out.print(""); // 空文字をセット
+      }
+
+    }
+
+    public void DisplayClear( int hight ){
+
+      for (int i = 0; i < hight; i++) {
+        System.out.print("\033[1A"); // カーソルを1行上に移動
+        System.out.print("\033[2K"); // 現在の行をクリア
+      }
+
     }
     
 }
